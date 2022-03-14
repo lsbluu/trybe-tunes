a1c6442a073caf2b49c112cb5d360cfbd8c10a37
Lsbluu <lsblu@hotmail.com>
2022-03-14 16:29:47 -0300

    REQ5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,62 @@ import React, { Component } from 'react';
 import Header from '../component/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      disabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleValidate = this.handleValidate.bind(this);
+  }
+
+  handleValidate() {
+    const { name } = this.state;
+    const minLength = 2;
+    const isMin = name.length >= minLength;
+
+    this.setState({
+      disabled: !isMin,
+    });
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+
+    this.setState(
+      {
+        name: value,
+      },
+      this.handleValidate,
+    );
+  }
+
   render() {
+    const { name, disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+
+        <form>
+          <label htmlFor="name">
+            <input
+              id="name"
+              data-testid="search-artist-input"
+              type="text"
+              value={ name }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ disabled }
+          >
+            Pesquisar
+          </button>
+        </form>
+
       </div>
     );
   }
